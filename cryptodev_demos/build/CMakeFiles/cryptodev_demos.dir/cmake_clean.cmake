ba8d104ddc74b4e9ef06df2aa26bb06551354e46
eangelou <eangelou@cslab.ece.ntua.gr>
2012-10-31 14:07:36 +0200

    Added cryptodev

---
+++ cryptodev_demos/build/CMakeFiles/cryptodev_demos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cryptodev_demos.dir/main.o"
+  "cryptodev_demos.pdb"
+  "cryptodev_demos"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cryptodev_demos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
