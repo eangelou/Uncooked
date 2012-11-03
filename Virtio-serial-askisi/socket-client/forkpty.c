4e56f981d15617df1f3cb33499e3870ca80e23ca
eangelou <eangelou@cslab.ece.ntua.gr>
2012-11-03 20:34:07 +0200

    fixed for general release

--- Virtio-serial-askisi/socket-client/forkpty.c
+++ Virtio-serial-askisi/socket-client/forkpty.c
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <sys/stream.h>
+// #include <sys/stream.h>
 #include <sys/stropts.h>
 
 int forkpty (int *amaster, char *name, void *unused1, void *unused2)
@@ -24,27 +24,37 @@ int forkpty (int *amaster, char *name, void *unused1, void *unused2)
     int master, slave;
     char *slave_name;
     pid_t pid;
+    
+    setsid();
 
-    master = open( "/dev/ptmx", O_RDWR );
-    if ( master < 0 )
+//     master = open("/dev/ptmx", O_RDWR|O_NOCTTY);
+    master = posix_openpt(O_RDWR);
+    if ( master < 0 ){
+	printf("master error");
+	perror("forkpty error1");
         return -1;
+    }
 
+    printf("master: %d\n",master);
     if ( grantpt( master ) < 0 )
     {
         close( master );
+	perror("forkpty error2");
         return -1;
     }
 
     if ( unlockpt( master ) < 0 )
     {
         close( master );
+	perror("forkpty error3");
         return -1;
     }
-
+    
     slave_name = ptsname( master );
     if ( slave_name == NULL )
     {
         close( master );
+	perror("forkpty error4");
         return -1;
     }
 
@@ -52,14 +62,17 @@ int forkpty (int *amaster, char *name, void *unused1, void *unused2)
     if (slave < 0)
     {
         close( master );
+	perror("forkpty error5");
         return -1;
     }
+    printf("slave: %d\n",slave);
 
     if (ioctl( slave, I_PUSH, "ptem" )   < 0
     ||  ioctl( slave, I_PUSH, "ldterm" ) < 0)
     {
         close( slave );
         close( master );
+	perror("forkpty error6");
         return -1;
     }
 
@@ -74,6 +87,7 @@ int forkpty (int *amaster, char *name, void *unused1, void *unused2)
     {
         case -1:      /* Error */
             return -1;
+	    perror("forkpty error7");
         case  0:      /* Child */
             close (master);
             dup2 (slave, STDIN_FILENO);
@@ -82,8 +96,23 @@ int forkpty (int *amaster, char *name, void *unused1, void *unused2)
             return 0;
         default:      /* Parent */
             close (slave);
+	    perror("forkpty error8");
             return pid;
     }
 
     return -1;
+}
+
+int
+login_tty(int fd)
+{
+	(void) setsid();
+	if (ioctl(fd, TIOCSCTTY, (char *)NULL) == -1)
+		return (-1);
+	(void) dup2(fd, 0);
+	(void) dup2(fd, 1);
+	(void) dup2(fd, 2);
+	if (fd > 2)
+		(void) close(fd);
+	return (0);
 }
